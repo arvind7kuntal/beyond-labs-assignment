97e49453a7db9864fbc04bcce8f909301590fe01
arvind7kuntal <arvind7kuntal@gmail.com>
2026-04-28 12:50:16 +0530

    Clean project - ready for assignment

--- src/main/java/com/beyondlabs/backend_assignment/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.beyondlabs.backend_assignment.exception;
-
-public class UserNotFoundException extends RuntimeException{
-
-    public UserNotFoundException(String message){
-        super(message);
-    }
-}
